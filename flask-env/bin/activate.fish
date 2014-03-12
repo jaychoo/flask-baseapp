2ea51fd0f875ec89865d4c2ab31bf788b42c2a3e
James Quacinella <james@deemok.com>
2014-03-11 23:32:00 -0400

    The environment was completely fucked up, since I copied it from another directory. Redid it.

--- flask-env/bin/activate.fish
+++ flask-env/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/james/Development/Credacious.com/credacious-flask/flask-env"
+set -gx VIRTUAL_ENV "/home/james/Development/Flask/flask-baseapp/flask-env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
