30c66ce15a1155ee6bcd0305a5b8f4328aadde23
James Quacinella <james@deemok.com>
2014-03-17 14:23:56 -0400

    Now using IPython for the shell with a custom message.

--- shell.py
+++ shell.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 import os
-import readline
-from pprint import pprint
+import IPython
 
-from flask import *
+# Import our app
 from app import *
 
 os.environ['PYTHONINSPECT'] = 'True'
+
+# Setup the IPython welcome message
+welcome_message = """Welcome to your Flask CLI environment.
+The following variables are available to use:
+
+app -> Your Flask app instance
+db -> Your Flask db instance
+mail -> Your Flask mail instance
+"""
+
+# Start IPython
+IPython.embed(header=welcome_message)
