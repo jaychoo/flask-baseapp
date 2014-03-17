5fd7fa3147b66c8f450e2fe960a847d6381aad04
James Quacinella <james@deemok.com>
2014-03-17 13:26:31 -0400

    Updating mail setup and configuration.

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,26 @@
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
+# Create Flask App Instance
 app = Flask(__name__)
 app.config.from_object('app.config')
 
+# Create DB object
 db = SQLAlchemy(app)
 
+# Create Mail object
+mail = Mail(app)
+
+# Setup 404 handler
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
 
-from app.users.views import mod as usersModule
-app.register_blueprint(usersModule)
-
 # Later on you'll import the other blueprints the same way:
+# from app.users.views import mod as usersModule
 #from app.comments.views import mod as commentsModule
 #from app.posts.views import mod as postsModule
+# app.register_blueprint(usersModule)
 #app.register_blueprint(commentsModule)
 #app.register_blueprint(postsModule)
--- app/config.py
+++ app/config.py
@@ -6,16 +6,27 @@ DEBUG = False
 ADMINS = frozenset(['youremail@yourdomain.com'])
 SECRET_KEY = 'SecretKeyForSessionSigning'
 
+# SQLAlchemy Options
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(_basedir, 'db_repository')
 DATABASE_CONNECT_OPTIONS = {}
 
 THREADS_PER_PAGE = 8
 
+# CSRF Settings
 CSRF_ENABLED = True
 CSRF_SESSION_KEY = "somethingimpossibletoguess"
 
+# Recaptcha options
 RECAPTCHA_USE_SSL = False
 RECAPTCHA_PUBLIC_KEY = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
 RECAPTCHA_PRIVATE_KEY = '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
 RECAPTCHA_OPTIONS = {'theme': 'white'}
+
+# SMTP Settings
+MAIL_SERVER = "email-smtp.us-east-1.amazonaws.com"
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = "SMTP username"
+MAIL_PASSWORD = "SMTP password"
+MAIL_DEFAULT_SENDER = "do-not-reply@credacious.com"
